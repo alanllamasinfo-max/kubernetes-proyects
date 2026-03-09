fa44d941330453c1a7eaae7d8fcaecbfa6141def
Alexito <alan.llamas.info@gmail.com>
2026-03-09 12:01:20 +0100

    API-REDIS

--- update-web.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-kubectl delete configmap web-content
-kubectl create configmap web-content --from-file=index.html
-kubectl rollout restart deployment nginx-frontend
-echo "🚀 Frontend actualizado y reiniciado"
